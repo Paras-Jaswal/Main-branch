2b4ff49f66e7c10e02beef639a0a52faede28337
Piyush <avpiyush2@gmail.com>
2024-07-16 13:03:32 +0530

    main-change

--- portfolio/front-end/src/Components/Signup & Login/Headers.jsx
+++ portfolio/front-end/src/Components/Signup & Login/Headers.jsx
@@ -8,8 +8,8 @@ function Headers() {
     <div className="d-flex  ">
         <NavLink to="/" className="text-decoration-none text-white pt-2">Portfolio</NavLink>
 <div className='ms-auto d-flex column-gap-5 pt-2'>
-        <NavLink to="/sign-up/investor" className="text-decoration-none text-white">Investor</NavLink>
-        <NavLink to="/sign-up/wealth" className="text-decoration-none text-white">Wealth Proffesionals</NavLink>
+        <NavLink to="/sign-up/investor" className="text-decoration-none text-white navs">Investor</NavLink>
+        <NavLink to="/sign-up/wealth" className="text-decoration-none text-white navs">Wealth Proffesionals</NavLink>
         </div>
         <div className="ms-auto d-flex column-gap-3">
         <p className='pt-2 text-primary'>Already have an account?</p> 
