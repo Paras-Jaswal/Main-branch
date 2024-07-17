63c1e159c591464dc881af244561a55f02d908cb
Paras Jaswal <154978460+Paras-Jaswal@users.noreply.github.com>
2024-07-17 13:33:45 +0530

    fix the issue

--- portfolio/backend/login.php
+++ portfolio/backend/login.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
@@ -9,13 +10,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit(0);
 }
 
-include 'connection.php';
-
+include 'connection.php'; 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = json_decode(file_get_contents('php://input'), true);
 
-    $email = filter_var($data['email'], FILTER_SANITIZE_EMAIL);
-    $password = filter_var($data['password'], FILTER_SANITIZE_STRING);
+    $email = $data['email'];
+    $password = $data['password'];
 
     $stmt = $conn->prepare("SELECT * FROM users WHERE Email = :email");
     $stmt->bindParam(':email', $email);
@@ -23,10 +23,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
     if ($user && password_verify($password, $user['Password'])) {
-        // Start session and set user ID
-        $_SESSION['user_id'] = $user['id']; // Assuming 'id' is the user ID column in your database
-
-        echo json_encode(['status' => 'success', 'message' => 'Login successful']);
+        $_SESSION['user_id'] = $user['Id'];
+        $current_user_id = $_SESSION['user_id'];
+        echo json_encode(['status' => 'success', 'message' => 'Login successful' , 'Id' => $current_user_id]);
     } else {
         echo json_encode(['status' => 'error', 'message' => 'Invalid email or password']);
     }
