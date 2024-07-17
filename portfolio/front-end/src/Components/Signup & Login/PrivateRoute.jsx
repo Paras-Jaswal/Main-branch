3d59eabe0f9930adc2b1f7b16f4d11e60428de38
Piyush <avpiyush2@gmail.com>
2024-07-17 11:22:52 +0530

    changes

--- portfolio/front-end/src/Components/Signup & Login/PrivateRoute.jsx
+++ portfolio/front-end/src/Components/Signup & Login/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 // PrivateRoute.jsx
 import React from 'react';
 import { Navigate, Route } from 'react-router-dom';
-import { isAuthenticated } from './auth'; // Adjust the path as per your file structure
+import { isAuthenticated } from '../Auth/Auth'; // Adjust the path as per your file structure
 
 const PrivateRoute = ({ element, ...rest }) => {
   return isAuthenticated() ? (
